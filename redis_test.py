03dfa5e61798facc89349e740a34733ec3a7f175
djskl <448189234@qq.com>
2017-02-01 10:33:23 +0800

    读写测试

--- redis_test.py
+++ redis_test.py
@@ -1,28 +1,74 @@
 import unittest
 import redis
-from StringIO import StringIO
 from redisweeds.base import WeedFS
+from redisweeds.exceptions import FileNotExists, WeedDuplicateError
 from redisweeds.utils import make_weed_key
 from redisweeds.weed import RedisWeed
 
 wfs = WeedFS()
 rconn = redis.StrictRedis()
 
-class TestWeedRead(unittest.TestCase):
+class TestWeedWrite(unittest.TestCase):
+
     def setUp(self):
-        self.cnt = "123"
-        self.normal_name = make_weed_key("normal_f")
-
-        rconn.set(self.normal_name, wfs.upload_file(
-            name=self.normal_name,
-            stream=StringIO(self.cnt)
-        ))
-
         self.rd = RedisWeed()
+        self.test_filename = "test_write_filename"
+        self.test_filekey = make_weed_key(self.test_filename)
+        self.test_cnt_1 = "ces1"
+        self.test_cnt_2 = "ces2"
+
+    def tearDown(self):
+        wfs.delete_file(rconn.get(self.test_filekey))
+        rconn.delete(self.test_filekey)
+
+    def test_write(self):
+        self.rd.write(self.test_filename, self.test_cnt_1)
+
+        self.assertTrue(rconn.exists(self.test_filekey))
+
+        ret = wfs.get_file(rconn.get(self.test_filekey))
+        self.assertEqual(self.test_cnt_1, ret)
+
+    def test_no_overwrite(self):
+        self.rd.write(self.test_filename, self.test_cnt_1)
+
+        with self.assertRaises(WeedDuplicateError):
+            self.rd.write(self.test_filename, self.test_cnt_2)
+
+    def test_overwrite(self):
+
+        self.rd.write(self.test_filename, self.test_cnt_1)
+        self.rd.write(self.test_filename, self.test_cnt_2, True)
+
+        ret = wfs.get_file(rconn.get(self.test_filekey))
+        self.assertEqual(self.test_cnt_2, ret)
+
+
+class TestWeedRead(unittest.TestCase):
+
+    def setUp(self):
+        self.rd = RedisWeed()
+        self.test_filename = "test_read_filename"
+        self.test_filekey = make_weed_key(self.test_filename)
+        self.test_cnt = "ces"
+
+        self.rd.write(self.test_filename, self.test_cnt)
+
+    def tearDown(self):
+        wfs.delete_file(rconn.get(self.test_filekey))
+        rconn.delete(self.test_filekey)
 
     def test_read(self):
-        rst = self.rd.read(self.normal_name)
-        self.assertEqual(self.cnt, rst)
+        ret = self.rd.read(self.test_filename)
+        self.assertEqual(self.test_cnt, ret)
+
+    def test_read_no_exists(self):
+        with self.assertRaises(FileNotExists) as cm:
+            self.rd.read("no_exists_filename")
+
+        the_exception = cm.exception
+        self.assertEqual(the_exception.filename, "no_exists_filename")
+
 
 if __name__ == "__main__":
     unittest.main()
